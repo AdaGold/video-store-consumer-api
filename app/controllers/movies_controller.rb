17ab24262d5240620507e56b39f1e5c9ed637dd5
Ari <dominiquearia0@gmail.com>
2018-06-20 12:45:23 -0700

    working on movie controller create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,15 @@ class MoviesController < ApplicationController
   end
 
   def create
+    # raw_movie = params[:movie]
+    # movie.title = raw_movie[:title]
+    # movie.overview = raw_movie[:overview]
+    # movie.release_date = raw_movie[:release_date]
+    # movie.image_url = raw_movie[:image_url].slice!('https://image.tmdb.org/t/p/w185')
+    # movie.external_id = raw_movie[:external_id]
+    # movie.inventory = raw_movie[:inventory]
+    #
+    # movie = Movie.new(raw_movie)
     movie = Movie.new(movie_params)
     if movie.save
       render json: {
