e672c898abc3b20a8c9f3f9c47b55a1afebb993c
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-06-20 12:50:50 -0700

    updated create method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,19 +11,9 @@ class MoviesController < ApplicationController
   end
 
   def create
-    # raw_movie = params[:movie]
-    # movie.title = raw_movie[:title]
-    # movie.overview = raw_movie[:overview]
-    # movie.release_date = raw_movie[:release_date]
-    # movie.image_url = raw_movie[:image_url].slice!('https://image.tmdb.org/t/p/w185')
-    # movie.external_id = raw_movie[:external_id]
-    # movie.inventory = raw_movie[:inventory]
-    #
-    # movie = Movie.new(raw_movie)
     movie = Movie.new(movie_params)
     if movie.save
-      render json: {
-        id: movie.id }, status: :ok
+      render json: movie.as_json(except: [:created_at, :updated_at], status: :ok)
     else
       render json: { errors: movie.errors.messages }, status: :bad_request
     end
